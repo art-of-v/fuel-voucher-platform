f1f67dc12ebf4fd77ab0b10d8303d3d6cf47240c
Artem Vashchuk <artem.vashchuk@gmail.com>
2026-07-05 13:07:10 +0300

    cleanup: remove old Node.js backend files and related config leftovers

--- admin-panel/backend/src/application/services/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Application Services Barrel Export
- */
-
-export * from './auth.service';
-export * from './purchase.service';
-export * from './fulfillment.service';
-export * from './user.service';
-export * from './voucher.service';
